c594a843e531a0074524ef28285e8c147036862f
Ibrahim Afzal Shareef <ibrahim.afzal1999@gmail.com>
2026-07-09 17:00:08 +0500

    Added SSE fetch code (commented)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,3 +7,39 @@ export function base64ToBytes(base64String: string) {
   }
   return byteNumbers;
 }
+
+// const res = await fetch(`/api/v3/hylscraper/${sessionId}/stream`, {
+//   headers: { Authorization: `Bearer ${token}` },
+// });
+//
+// const reader = res.body!.getReader();
+// const decoder = new TextDecoder();
+// let buffer = "";
+//
+// while (true) {
+//   const { done, value } = await reader.read();
+//   if (done) break;
+//
+//   buffer += decoder.decode(value, { stream: true });
+//
+//   // SSE events are separated by double newline
+//   const events = buffer.split("\n\n");
+//   buffer = events.pop()!; // keep incomplete last chunk
+//
+//   for (const event of events) {
+//     const lines = event.split("\n");
+//     let eventType = "message";
+//     let data = "";
+//
+//     for (const line of lines) {
+//       if (line.startsWith("event: ")) eventType = line.slice(7);
+//       if (line.startsWith("data: ")) data = line.slice(6);
+//     }
+//
+//     if (eventType === "done") { /* stream finished */ break; }
+//     if (data) {
+//       const post = JSON.parse(data);
+//       // do something with post
+//     }
+//   }
+// }
